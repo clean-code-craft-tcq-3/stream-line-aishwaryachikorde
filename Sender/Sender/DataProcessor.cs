1c1457ec2978ca8b5df958f8a7b450d3753ed3cd
Aishwarya Chikorde <41798723+aishwaryachikorde@users.noreply.github.com>
2022-07-13 15:03:27 +0530

    Update DataProcessor.cs

--- Sender/Sender/DataProcessor.cs
+++ Sender/Sender/DataProcessor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Reflection;
+using System.Text;
 using System.Text.Json;
 
 namespace Sender
@@ -17,11 +18,38 @@ namespace Sender
       LogSensorReadingsOnConsole(sensorDataReadings);
     }
 
+    private static void CreateNewCsvFile(string fileDirectory)
+    {
+      var newFile = File.Create(Path.Combine(fileDirectory, "SensorReading.csv"));
+      newFile.Close();
+    }
+
+    private static void GenerateSensorData(string fileDirectory)
+    {
+      Random temperatureData = new Random();
+      Random stateOfChargeData = new Random();
+      using StreamWriter writer = File.AppendText(Path.Combine(fileDirectory, "SensorReading.csv"));
+      for (int i = 1; i <= 50; i++)
+      {
+        int number1 = temperatureData.Next(0, 45);
+        int number2 = stateOfChargeData.Next(20, 80);
+        writer.Write(number1 + "," + number2 + "\n");
+      }
+    }
+
     public static List<SensorParameter> ReadInputData()
     {
+      string workingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+      if (workingDirectory != null)
+      {
+        CreateNewCsvFile(workingDirectory);
+        GenerateSensorData(workingDirectory);
+      }
+
       List<SensorParameter> inputReadingList = new List<SensorParameter>();
 
-       string fileDirectory = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "SensorReading.csv");
+      string fileDirectory = Path.Combine(workingDirectory, "SensorReading.csv");
 
       using StreamReader streamReader = new StreamReader(fileDirectory);
       while (!streamReader.EndOfStream)
@@ -33,21 +61,18 @@ namespace Sender
       return inputReadingList;
     }
 
-    public static string ConvertInputToJsonFormat(SensorParameter sensorReading)
+    public static string ConvertInputToJsonFormat(List<SensorParameter> sensorReading)
     {
       string jsonFormatReading = JsonSerializer.Serialize(sensorReading);
 
       return jsonFormatReading;
     }
 
-    public static void LogSensorReadingsOnConsole(List<SensorParameter> sensorDataReadings)
+    private static void LogSensorReadingsOnConsole(List<SensorParameter> sensorDataReadings)
     {
-      foreach (SensorParameter sensorReading in sensorDataReadings)
-      {
-        string readingInJson = ConvertInputToJsonFormat(sensorReading);
-
-        Console.WriteLine(readingInJson);
-      }
+       string readingInJson = ConvertInputToJsonFormat(sensorDataReadings); 
+       Console.WriteLine(readingInJson);
     }
   }
 }
+
